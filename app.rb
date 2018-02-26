41f0353e002c183633293ee7cb37d42eb36c437c
Daniel Keen <keendaniel01@gmail.com>
2018-02-26 15:27:24 +0000

    User can sign up, and see welcome message

--- app.rb
+++ app.rb
@@ -1,17 +1,20 @@
 ENV["ENVIRONMENT"] ||= "development"
 require 'sinatra/base'
 require './lib/property'
+require './lib/user'
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
 
 class MakersBnb < Sinatra::Base
+  enable :sessions
 
   get '/' do
     redirect "/index"
   end
 
   get '/index' do
+    @user = session[:user]
     erb(:index)
   end
 
@@ -20,6 +23,12 @@ class MakersBnb < Sinatra::Base
     @picture = params[:picture]
     erb :add_listing
     # redirect '/index'
+  end
+
+  post '/new-user' do
+    session[:user] = params[:name]
+    User.create(name: params[:name], email: params[:signupemail], password: params[:password])
+    redirect '/index'
   end
 
 end
