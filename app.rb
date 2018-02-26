102b4a8e66548d0f077fbedf42276e051cf4f281
jose <jennymarin1989@hotmail.com>
2018-02-26 12:31:54 +0000

    Create get and post route and views

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
 class MakersBnb < Sinatra::Base
-  
-  get '/' do 
+
+  get '/' do
+    redirect "/index"
+  end
+
+  get '/index' do
     erb(:index)
+  end
+
+  post '/add-listing' do
+    @email = params[:email]
+    @picture = params[:picture]
+    erb :add_listing
+    # redirect '/index'
   end
 
 end
--- spec/features/sinatra_test_spec.rb
+++ spec/features/sinatra_test_spec.rb
@@ -1,6 +1,9 @@
-feature 'testing initial setup' do
-  scenario 'home page has welcome message' do
+feature 'Displaying listings' do
+  scenario 'Landlord can fill in with information' do
     visit '/'
-    expect(page).to have_content("Welcome to Makers BnB!")
+    fill_in "email", with: "john@example.com"
+    fill_in "picture", with: "https://imgur.com/rE8X4ir"
+    click_button "submit"
+    expect(page).to have_content("john@example.com")
   end
 end
