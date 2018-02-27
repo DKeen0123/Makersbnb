660e0141123cc7bbe895cf45e7a247b45ee1aa1e
jose <jennymarin1989@hotmail.com>
2018-02-27 11:15:44 +0000

    Adding react test

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div className="hello">
+        Hello, {this.props.name}!
+      </div>
+    );
+  }
+}
+
+global.Hello = Hello;
