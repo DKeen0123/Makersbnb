ed13d4c4b3879544aefbe7eca3e86ccd29db23c1
carlostrapet <trapet_llamas@hotmail.com>
2018-02-27 12:40:24 +0000

    authentication functionality complete || need to implement it into app

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'bcrypt'
 
 class User
 
@@ -8,14 +9,23 @@ class User
   property :id, Serial
   property :name, String
   property :email, String
-  property :password, String
+  property :password_digest, Text
 
 
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
-
-
-
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
 end
+
 
 DataMapper.setup(:default, "postgres://localhost/bnb_#{ENV['ENVIRONMENT']}")
 DataMapper.finalize
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -6,4 +6,11 @@ describe User do
       expect(described_class.all.first.name).to eq("mike")
     end
   end
+
+  describe ".authenticate" do 
+    it "logs in existing user if email/password combination exists" do 
+      user = described_class.create(name: "josh", email: "josh@test.com", password: "123")
+      expect(User.authenticate(user.email, '123')).to eq user
+    end
+  end
 end
